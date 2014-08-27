6affadea90963a096d9327eaafa3b6cbcd85be16
Anthony Corbin <dragon1672@gmail.com>
2014-08-27 09:04:08 -0600

    Better comments for exampleLevel

--- FallDown/LevelClass.js
+++ FallDown/LevelClass.js
@@ -1,6 +1,6 @@
 /*global createjs */
 /*global mouse */
-function Level (title) {
+function LevelExample (title) {
     //Game state
     this.title = title;
     this.container = new createjs.Container();
@@ -12,6 +12,7 @@ function Level (title) {
             this.currentItem.graphic.x = mouse.pos.x;
             this.currentItem.graphic.y = mouse.pos.y;
         }
+        //put other general GUI stuff here
         this.levelUpdate();
     };
 
@@ -19,11 +20,22 @@ function Level (title) {
     this.grid = null;
     this.currentItem = null;
     this.nextItems = [];
-    this.mouseIsDown = false;
+    this.turns = 50;
 
     this.levelUpdate = function() { };
     this.mouseDownEvent = function(e) { e=e; };
-    this.mouseUpEvent = function(e) { e=e; };
-    this.enable  = function() { };
+    this.mouseUpEvent = function(e) {
+        e=e;
+        //find closest square
+        //if this.currentItem.onPlace()
+            //this.turns--
+    };
+    this.enable  = function() {
+        //this.grid = new Grid();
+            //setup grid
+        //change turns to be different number?
+        //init nextItems
+        //set current item
+    };
     this.disable = function() { };
 }
