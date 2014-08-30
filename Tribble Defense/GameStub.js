9b84df91ebe94945eb2fbce9397e3331d07d014d
Anthony Corbin <dragon1672@gmail.com>
2014-08-29 18:07:29 -0600

    Stub updated with better info

--- Tribble Defense/GameStub.js
+++ Tribble Defense/GameStub.js
@@ -1,7 +1,12 @@
+function Coord() {} // included from main project
+
+
 var ItemType = { // enum
     Hazard: "haz",
     Housing: "woody",
-    Static: "oldTv"
+    Static: "oldTv",
+    ComboMaster: "combine with all",
+    BlackHole: "Anything will die",
 };
 
 function Cell(pos) {
@@ -12,7 +17,7 @@ function Cell(pos) {
 
 function Query(valid) {
     this.levelBoost = 0;
-    this.positions = [];
+    this.positions = [new Coord(), new Coord(1,1)];
     this.valid = valid;
 }
 
@@ -21,11 +26,12 @@ function Item(type) {
     this.population = 0;
     this.direction = new Coord();
     this.strength = 0;
+    this.type = type;
 }
 
 function Spawner() {
     this.pos = new Coord();
-    this.directions = [];
+    this.directions = [new Coord(), new Coord()];
     this.freqLow = 5;
     this.freqHigh = 10;
     this.turnsTillNextSpawn = 7; // will be updated based off freq
@@ -43,14 +49,21 @@ function Spawner() {
 function Game(size) { // pass in Coord of size
     this.getTurnCount  = function() { return size; };
     this.itemQ         = function(index) {
+        index = index;
         var ret = new Item(ItemType.Housing);
         ret.population = 1;
         return ret;
     };
     this.QueryMove     = function(pos,itemToPlace) {
+        pos = pos;
+        itemToPlace = itemToPlace;
         var ret = new Query(true);
     };
-    this.ApplyMove     = function(pos,itemToPlace) { return this.QueryMove(); };
+    this.ApplyMove     = function(pos,itemToPlace) { 
+        pos = pos;
+        itemToPlace = itemToPlace;
+        return this.QueryMove();
+    };
     this.getCell       = function(pos) {
         return new Cell(pos);
     };
@@ -58,7 +71,7 @@ function Game(size) { // pass in Coord of size
     this.update = function() {};
     
     //for building map
-    this.setComboBoost = function(boost) {};
-    this.setSpawner = function(pos,spawner) {};
+    this.setComboBoost = function(boost) { boost = boost; };
+    this.setSpawner = function(pos,spawner) { pos = pos; spawner = spawner;};
     this.getDims = function() { return size; };
 }
