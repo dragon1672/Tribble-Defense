ba7485b8cc67ccffb471b00f24b4d7bb012c1095
DaleChetney <dalechetney@gmail.com>
2014-09-05 00:17:39 -0600

    event driven implementation

--- Tribble Defense/game.js
+++ Tribble Defense/game.js
@@ -713,6 +713,167 @@ var HashSet = (function() {
 })();
 
 //endregion
+//region hasy
+
+/*
+ * Hash table developed by Anthony Corbin
+//*/
+var HashTable, HashMap;
+ HashTable = HashMap = (function() {
+	function HashTable() {
+		this.pairs = [];
+		this.orderedPairs = [];
+	}
+	function KeyValuePair(hash, key, val) {
+		this.hash = hash;
+		this.key = key;
+		this.val = val;
+        this.markedForDel = false;
+	}
+    
+    var hasher = function (value) {
+        return (typeof value) + ' ' + (value instanceof Object ? (value.__hash || (value.__hash = ++arguments.callee.current)) : value.toString());
+    };
+    hasher.current = 0;
+    
+    this.numOfActiveIterations = 0;
+    
+    HashTable.prototype.hashObject = hasher;
+	KeyValuePair.prototype.containsKey = function (key) { return this.key === key; };
+	KeyValuePair.prototype.containsVal = function (val) { return this.val === val; };
+	HashTable.prototype.add = function (newKey, newVal) {
+		var hash = this.hashObject(newKey);
+		if (!this.containsKey(newKey)) {
+			this.pairs[hash] = new KeyValuePair(hash, newKey, newVal);
+			this.orderedPairs.push(this.pairs[hash]);
+		} else {
+			this.pairs[hash].val = newVal;
+		}
+	};
+	HashTable.prototype.put  = this.add;
+	HashTable.prototype.get = function (key) {
+		var hash = this.hashObject(key);
+		if (this.pairs[hash] !== null) { return this.pairs[hash].val; }
+		return null;
+	};
+	HashTable.prototype.remove = function (key) {
+		var i, hash;
+		if (this.containsKey(key)) {
+			hash = this.hashObject(key);
+            this.pairs[hash].markedForDel = true;
+            var del = function del() {
+                if(this.numOfActiveIterations > 0) {
+                    setTimeout(del,10);
+                    return;
+                }
+                for (i = 0; i < this.orderedPairs.length; i++) {
+                    if (this.orderedPairs[i] === this.pairs[hash]) {
+                        this.orderedPairs.splice(i, 1);
+                        this.pairs[hash] = null;
+                        return;
+                    }
+                }
+                throw new Error("contain returned true, but key not found");
+            };
+		}
+	};
+	HashTable.prototype.containsKey = function (key) {
+		var hash = this.hashObject(key);
+		return (this.pairs[hash] && (this.pairs[hash] instanceof KeyValuePair)) ? true : false;
+	};
+	HashTable.prototype.containsValue = function (val) {
+		var ret = false;
+		this.orderedPairs.map(function (item) {
+			ret = ret || item.val === val;
+		});
+		return ret;
+	};
+	HashTable.prototype.isEmpty = function () { return this.size() === 0; };
+	HashTable.prototype.size = function () { return this.orderedPairs.length; };
+	//pass in function(key,val)
+	HashTable.prototype.foreachInSet = function (theirFunction) {
+        this.numOfActiveIterations++;
+		this.orderedPairs.map(function (item) {
+            if(!item.markedForDel) {
+                theirFunction(item.key, item.val);
+            }
+		});
+        this.numOfActiveIterations--;
+	};
+	return HashTable;
+}());
+
+/*
+ * Hash Set developed by Anthony Corbin
+//*/
+var HashSet = (function() {
+	function HashSet() {
+		this.myTable = new HashTable();
+	}
+	HashSet.prototype.add      = function (val)      { return this.myTable.add(val, true);       };
+	HashSet.prototype.addAll   = function (vals)     { var potato = this; vals.map(function(item) { potato.myTable.add(item,true); }); };
+	HashSet.prototype.contains = function (toCheck)  { return this.myTable.containsKey(toCheck); };
+	HashSet.prototype.remove   = function (toRemove) { return this.myTable.remove(toRemove);     };
+	HashSet.prototype.size     = function ()         { return this.myTable.size(); };
+    
+    HashSet.prototype.cross = function (that) {
+		var ret = new HashSet();
+		this.foreachInSet(function (a) {
+            that.foreachInSet(function (b) {
+                var toAdd = {
+                    0: a,
+                    1: b,
+                };
+                ret.add(toAdd);
+            });
+        });
+		return ret;
+	};
+	HashSet.prototype.union = function (that) {
+		var ret = new HashSet();
+		this.foreachInSet(function (item) { ret.add(item); });
+		that.foreachInSet(function (item) { ret.add(item); });
+		return ret;
+	};
+	HashSet.prototype.join  = function (that) {
+		var ret = new HashSet();
+		this.myTable.foreachInSet(function (key, val) {
+			if (that.contains(key)) { ret.add(key); }
+		});
+		return ret;
+	};
+    HashSet.prototype.removeSet = function (that) {
+        that.foreachInSet(function(item) {
+            this.remove(item); 
+        });
+	};
+    HashSet.prototype.isEqual   = function (that) {
+		return this.isSubsetOf(that) && that.isSuperSet(this);
+	};
+    HashSet.prototype.isSubSet  = function (that) {
+		var ret = true;
+		this.myTable.foreachInSet(function (item) {
+            ret = ret && that.contains(item);
+		});
+		return ret;
+	};
+    HashSet.prototype.isSuperSet   = function (that) {
+        return that.isSubSet(this);
+	};
+	HashSet.prototype.foreachInSet = function (theirFunction) {
+		return this.myTable.foreachInSet(function(key,val) { theirFunction(key); });
+	};
+    HashSet.prototype.toList = function () {
+        var ret = [];
+		this.foreachInSet(function (item) {
+            ret.push(item);
+		});
+        return ret;
+	};
+    return HashSet;
+})();
+
+//endregion
 
 //region classes
 var ItemType = { // enum
@@ -744,10 +905,10 @@ function Item(type) {
     this.strength = 0;
     this.type = type;
     this.getLevel = function() {
-        return Math.max(0,Math.floor((Math.log(this.population) / Math.log(3)) + 1));
+        return this.strength;
     };
     this.setToLevel = function(level) {
-        this.population = Math.pow(3,(level-1));
+        this.strength = level;
     };
     this.duplicate = function() {
         var ret = new Item(this.type);
@@ -764,22 +925,32 @@ function Item(type) {
                this.strength === that.strength &&
                this.type === that.type;
     };
-}
-
-function Spawner() {
-    this.pos = new Coord();
-    this.directions = [];
-    this.freqLow = 5;
-    this.freqHigh = 10;
-    this.turnsTillNextSpawn = 7; // will be updated based off freq
-    //will be changed 
-    this.update = function() {
-        this.turnsTillNextSpawn--;
-        if(this.turnsTillNextSpawn < 0) {
-            this.turnsTillNextSpawn = 7;
-        }
+    this.decreaseLevel = function() {
+        this.population -= this.population / this.getLevel();
+        this.setToLevel(this.getLevel()-1);
     };
 }
+function Hazard(level) {
+    var ret = new Item(ItemType.Hazard);
+    ret.setToLevel(level);
+    return ret;
+}
+var GameEvent = (function(){
+    function GameEvent() {
+        this.calls = new HashSet();
+    }
+    GameEvent.prototype.callAll = function(a,b,c,d,e,f,g) {
+        this.calls.foreachInSet(function(item) { item(a,b,c,d,e,f,g); });
+    };
+    GameEvent.prototype.addCallBack = function(toAdd) {
+        this.calls.add(toAdd);
+    };
+    GameEvent.prototype.removeCallBack = function (toKill) {
+        this.calls.remove(toKill);
+    };
+    return GameEvent;
+}());
+
 
 //endregion
 
@@ -788,17 +959,39 @@ function Game(size) { // pass in Coord of size
     this.size = size;
     this.turns = 42;
     this.Grid = [];
-    this.ComboBoost = 0;
+    this.ComboBoost = 3;
     this.avalableItemPool = [];
+    
+    this.spawners = [];
+    this.trackedHazards = new HashSet();
+    
+    //region events
+    
+    //function(pos,oldItem, new item)
+    this.itemChangedEvent = new GameEvent();
+    //function(pos,hazard)
+    this.hazardSpawnedEvent = new GameEvent();
+    //function(oldPos,newPos)
+    this.hazardMovedEvent = new GameEvent();
+    //function(pos,hazard)
+    this.hazardRemovedEvent = new GameEvent();
+    
+    //function()
+    this.itemQChangedEvent = new GameEvent();
+    //function()
+    this.populationChangedEvent = new GameEvent();
+    
+    //endregion
+    
     //region init
     var i;
     { // init pool
         var basicHouse = new Item(ItemType.Housing);
         basicHouse.population = 1;
-        for(i = 0 ;i<5;i++) {
-            this.avalableItemPool.push(basicHouse.duplicate());
-        }
-        this.avalableItemPool.push(new Item(ItemType.BlackHole));
+        basicHouse.strength = 1;
+        this.addItemToPool(basicHouse,5);
+        //adding powerups
+        this.addItemToPool(new Item(ItemType.BlackHole),1);
     }
     this.nextItemList = [];
     
@@ -814,12 +1007,22 @@ function Game(size) { // pass in Coord of size
     
     //for building map
     this.setComboBoost = function(boost) { boost = boost; };
-    this.setSpawner = function(pos,spawner) { pos = pos; spawner = spawner;};
+    this.addSpawner = function(pos,spawner) { pos = pos; spawner = spawner;};
     this.getDims = function() { return size; };
 }
 
-
-
+Game.prototype.addHazard = function(toAdd) {
+    this.trackedHazards.add(toAdd);
+};
+Game.prototype.removeHazard = function(toKill) {
+    this.trackedHazards.remove(toKill);
+};
+Game.prototype.addItemToPool = function(item,count) {
+    count = count || 1;
+    for(var i = 0 ;i<count;i++) {
+        this.avalableItemPool.push(item.duplicate());
+    }
+};
 Game.prototype.foreachCell = function(operation) {
     for(var i=0;i<this.size.x;i++) {
         for(var j=0;j<this.size.y;j++) {
@@ -831,7 +1034,9 @@ Game.prototype.foreachCell = function(operation) {
 Game.prototype.getTurnCount  = function() { return this.turns; };
 Game.prototype.popFromQ = function() {
     this.itemQ(0);
-    return this.nextItemList.shift();
+    var ret = this.nextItemList.shift();
+    this.itemQChangedEvent.callAll();
+    return ret;
 };
 Game.prototype.itemQ         = function(index) {
     while(this.nextItemList.length <= index) {
@@ -853,11 +1058,9 @@ Game.prototype.QueryMove     = function(pos,itemToPlace) {
     var sameType;
     while( (sameType = this.MoveHelper(new HashSet(),this.getCell(pos),itemToCheck)).length >=3 ) {
         itemToCheck.setToLevel(itemToCheck.getLevel()+1);
-        ret.levelBoost++;
+        ret.levelBoost = itemToCheck.getLevel() - itemToPlace.getLevel();
         sameType.map(pushToRet);
     }
-    var temp = new HashSet();   temp.addAll(ret.positions); temp.remove(pos);       ret.positions = temp.toList();
-    temp = new HashSet();       temp.addAll(ret.cells);     temp.remove(thisCell);  ret.cells = temp.toList();
     ret.valid = ret.positions.length > 2;
     return ret;
 };
@@ -901,26 +1104,34 @@ Game.prototype.ApplyMove     = function(pos,itemToPlace, preloadedQuery) {
         
         this.avalableItemPool.push(itemToPlace.duplicate());
         
+        var potato = this;
         if(preloadedQuery.valid) {
             preloadedQuery.cells.map(function(meCell) {
                 if(!meCell.pos.isEqual(pos)) {
                     itemToPlace.population += meCell.item.population;
+                    var old = meCell.item;
                     meCell.item = null;
+                    potato.itemChangedEvent.callAll(meCell.pos,old,null);
                 }
             });
             itemToPlace.population += preloadedQuery.levelBoost * this.ComboBoost;
+            itemToPlace.setToLevel(itemToPlace.getLevel()+preloadedQuery.levelBoost);
+            this.populationChangedEvent.callAll();
         }
+        var old = thisCell.item;
         thisCell.item = itemToPlace;
+        this.itemChangedEvent.callAll(thisCell.pos,old,itemToPlace);
         this.avalableItemPool.push(itemToPlace.duplicate());
     } else {
         if(itemToPlace.type === ItemType.BlackHole) {
+            preloadedQuery = new Query(true);
             preloadedQuery.alreadyOccupied = thisCell.item !== null;
             thisCell.item = null;
-            preloadedQuery = new Query(true);
         }
     }
 
     this.turns--;
+    this.update();
 
     return preloadedQuery;
 };
@@ -944,12 +1155,126 @@ Game.prototype.getPopulation = function() {
     });
     return ret;
 };
+
+
+function Spawner(pos) {
+    this.pos = pos;
+    this.directions = [];
+    //power of hazard
+    this.powLow = 5;
+    this.posHigh = 6;
+    
+    //how often hazards are spawn
+    this.freqLow = 5;
+    this.freqHigh = 10;
+    this.turnsTillNextSpawn = Rand(this.freqLow,this.freqHigh); // will be updated based off freq
+    //will be changed 
+    this.updateTurns = function() {
+        this.turnsTillNextSpawn--;
+        if(this.turnsTillNextSpawn < 0) {
+            this.turnsTillNextSpawn = Rand(this.freqLow,this.freqHigh);
+            //spawn
+            var ret = new Hazard();
+            ret.direction = RandomElement(this.directions);
+            ret.setToLevel(Rand(this.powLow,this.powHigh));
+            return ret;
+        }
+        return null;
+    };
+}
+
+
 Game.prototype.update = function() {
-    console.log("lolz");
+    this.spawners.map(function(item) {
+        var newHazard = item.update();
+        if(newHazard !== null) {
+            
+            this.hazardSpawnedEvent.callAll(newHazard.pos,newHazard);
+        }
+    });
+    this.trackedHazards.foreachInSet(function(item) {
+        if(item === null) return;
+        var oldPos = item.pos;
+        item.pos = item.pos.add(item.direction);
+        this.hazardMovedEvent(oldPos,item.pos);
+        var cell = this.getCell(item.pos);
+        cell = new Cell();
+        if(cell !== null && cell.item !== null && cell.item.type === ItemType.Housing) {
+            //hazard beats item
+            var changed = false;
+            while(item.getLevel() > 0 && cell.item.getLevel() > 0) {
+                item.decreaseLevel();
+                cell.item.decreaseLevel();
+                changed = true;
+            }
+            if(changed) {
+                if(cell.item.getLevel() === 0) {
+                    var oldItem = cell.item;
+                    cell.item = null;
+                    this.itemChangedEvent.callAll(oldItem.pos,oldItem,cell.item);
+                }
+                if(item.getLevel() === 0) {
+                    this.hazardRemovedEvent.callAll(item.pos,item);
+                    this.removeHazard(item);
+                }
+            }
+        } 
+    });
 };
 
 
-//endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//message ticker
+
+
+//make a pool of possible message to poll from when Q is empty
+
+var MessageTicker = (function(){
+    function MessageTicker() {
+        this.characterQ = [];
+        this.dilimiter = " ";
+    }
+    MessageTicker.prototype.addMessage = function(stringMsg) {
+        var i;
+        for(i = 0;i<stringMsg.length;i++) {
+            this.characterQ.push(stringMsg.charAt(i));
+        }
+        for(i = 0;i<this.dilimiter.length;i++) {
+            this.characterQ.push(stringMsg.charAt(i));
+        }
+    };
+    MessageTicker.prototype.getString = function(length) {
+        length = length || 100;
+        var ret = "";
+        for(var i = 0;i<length && i< this.characterQ.length;i++) {
+            ret += this.characterQ[i];
+        }
+    };
+    MessageTicker.prototype.update = function() {
+        if(this.characterQ.length > 0) {
+            this.characterQ.shift();
+        }
+    };
+}());
 
 //region HUDOBJECT
 var terrainSprite;
@@ -1064,7 +1389,7 @@ var leftBar;
 var stars = [];
 
 function initGameScene(container) {
-    
+//region UI Init
     terrainSprite = loadImage("path");
     allGraphic[1] = loadImage("pop1");
     allGraphic[2] = loadImage("pop2");
@@ -1113,8 +1438,27 @@ QueueBorder[i].graphics.setStrokeStyle(5,"round").beginStroke("#333").drawRect(6
         stars[i].scaleX = 0.6+0.2*(i%2);
         stars[i].scaleY = 0.6+0.2*(i%2);
     }
+//endregion
     
     GameStates.Game.enable = function() {
+        
+//region Game Events
+        game = new Game(new Coord(6,6));
+        
+        game.itemQChangedEvent.addCallBack(function(e){
+            e = e;
+            updateQueue(container);
+        });
+        
+        game.populationChangedEvent.addCallBack(function(e){
+            e=e;
+            console.log(game.getPopulation());
+            pop.text = "Pop " +game.getPopulation();
+        });
+        
+//endregion
+        
+//region UI setup
         backgroundMusic.setSoundFromString("GamePlay",true);
         
         container.addChild(leftBar);
@@ -1133,13 +1477,6 @@ QueueBorder[i].graphics.setStrokeStyle(5,"round").beginStroke("#333").drawRect(6
         container.addChild(stars[0]);
         container.addChild(stars[1]);
         container.addChild(stars[2]);
-        
-        game = new Game(new Coord(6,6));
-        
-        game.itemQChangedEvent = function(event){
-            event = event;
-            updateQueue(container);
-        };
         
         turnsLabel = new createjs.Text("Turns: ", "italic 20px Orbitron", "#FFF");
         turnsLabel.x = 580;
@@ -1164,6 +1501,7 @@ QueueBorder[i].graphics.setStrokeStyle(5,"round").beginStroke("#333").drawRect(6
         updateQueue(container);
         
         grid = new Grid(container, new Coord(6,6),new Coord(30,50),new Coord(500,500));
+//endregion
     };
     GameStates.Game.mouseDownEvent = function(e){
         e=e;
@@ -1198,7 +1536,6 @@ QueueBorder[i].graphics.setStrokeStyle(5,"round").beginStroke("#333").drawRect(6
                             }
                         }
                     }
-                    pop.text = "Pop " +game.getPopulation();
                 }
             }
             else{
